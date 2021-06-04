46f80d07932f421df6b6405af544405db148ba5a
Ayush783 <ayushc45xxx@gmail.com>
2021-06-04 17:16:57 +0530

    created folder structure

---
+++ lib/country_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CountryApp extends StatelessWidget {
+  const CountryApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Country app',
+    );
+  }
+}
