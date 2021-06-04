7ae65c1140824fc0f092047fff276ce88648c971
Ayush783 <ayushc45xxx@gmail.com>
2021-06-04 18:50:35 +0530

    addd search result screen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -2,20 +2,20 @@ import 'package:country/constant/text_styles.dart';
 import 'package:country/constant/textfield_decoration.dart';
 import 'package:country/ui/home/providers/show_buttons.dart';
 import 'package:country/ui/home/widgets/home_screen_buttons.dart';
+import 'package:country/ui/search_result/search_result_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
-  static TextEditingController controller = TextEditingController();
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -54,14 +54,22 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ],
                             borderRadius: BorderRadius.circular(32)),
-                        child: TextField(
-                          controller: HomeScreen.controller,
+                        child: TextFormField(
+                          controller: controller,
                           decoration: kInputDecoration(size),
                         ),
                       ),
                       Padding(padding: EdgeInsets.only(top: size.height * 0.1)),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => SearchResultScreen(
+                                query: controller.text,
+                              ),
+                            ),
+                          );
+                        },
                         child: Text(
                           'Search',
                           style: kBodyTextStyle,
---
+++ lib/ui/search_result/search_result_screen.dart
@@ -0,0 +1,27 @@
+import 'package:country/constant/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class SearchResultScreen extends StatelessWidget {
+  final String? query;
+  const SearchResultScreen({Key? key, this.query}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+        child: Scaffold(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(
+            size.width * 0.05, size.width * 0.05, size.width * 0.05, 0),
+        child: Column(
+          children: [
+            Text(
+              'Showing Results for \"$query\"',
+              style: kheadingTextStyle,
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
