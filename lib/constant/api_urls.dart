c071f900a3e63ade11d336e59ec0eed91e961ae3
Ayush783 <ayushc45xxx@gmail.com>
2021-06-04 19:56:46 +0530

    added api urls

---
+++ lib/constant/api_urls.dart
@@ -0,0 +1,13 @@
+const String apiBaseUrl = 'https://restcountries.eu/rest/v2/';
+
+const String searchByNameUrl = apiBaseUrl + 'name/';
+
+const String searchByCodeUrl = apiBaseUrl + 'code/alpha';
+
+const String searchByCurencyUrl = apiBaseUrl + 'currency/';
+
+const String searchByLanguageUrl = apiBaseUrl + 'lang/';
+
+const String searchByCapitalUrl = apiBaseUrl + 'capital/';
+
+const String searchByRegionUrl = apiBaseUrl + 'region/';
