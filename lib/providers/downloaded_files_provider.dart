ed613c23d864b66e03943b584a8ab0740e18d739
Ayush783 <ayushc45xxx@gmail.com>
2021-06-05 11:36:26 +0530

    downloads provider implemented

---
+++ lib/providers/downloaded_files_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final downloadedFilesProvider =
+    ChangeNotifierProvider((ref) => DownloadsNotifier());
+
+class DownloadsNotifier extends ChangeNotifier {
+  final List<String> _value = List.empty(growable: true);
+  List<String> get value => _value;
+
+  void addFile(String name) {
+    _value.add(name);
+    notifyListeners();
+  }
+
+  void delete(String name) {
+    _value.remove(name);
+    notifyListeners();
+  }
+}
