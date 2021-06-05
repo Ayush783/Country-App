31294a2be5faa9f8608acf9cbb2d26fc118a2da2
Ayush783 <ayushc45xxx@gmail.com>
2021-06-05 15:10:41 +0530

    made app portrait only

--- lib/country_app.dart
+++ lib/country_app.dart
@@ -3,6 +3,7 @@ import 'package:country/constant/theme.dart';
 import 'package:country/repository/save/shared_pref_service.dart';
 import 'package:country/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CountryApp extends StatefulWidget {
   const CountryApp({Key key}) : super(key: key);
@@ -23,6 +24,18 @@ class _CountryAppState extends State<CountryApp> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      // To Prevent the App from going in Lanscape Mode, We lock it in Potrait
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      // This Allows us to change the Naviagtion and Status Bar Color of Device
+      statusBarColor: Color(0xff4285f4), //top bar color
+      statusBarIconBrightness: Brightness.light, //top bar icons
+      systemNavigationBarColor: Color(0xfff9f4f4), //bottom bar color
+      systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
+    ));
     return MaterialApp(
       title: 'Country app',
       home: HomeScreen(),
